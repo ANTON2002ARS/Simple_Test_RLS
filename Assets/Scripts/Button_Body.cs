211cdc785524cdfa69dfb92e490f46dfb1a0b9d7
__ANTON_2002__ <101361041+ANTON2002ARS@users.noreply.github.com>
2024-11-01 23:39:06 +0400

    Add Code

--- Assets/Scripts/Button_Body.cs
+++ Assets/Scripts/Button_Body.cs
@@ -5,15 +5,31 @@ using UnityEngine.UI;
 
 public class Button_Body : MonoBehaviour
 {
+    [SerializeField] private int Index;
     [SerializeField] private Text text_button;
     [SerializeField] private Text text_number;
+    private TestVariantAction VariantAction;
+
+    public void Start()=> this.GetComponent<Button>().onClick.AddListener(button_click);    
     
     public void Get_Variant(TestVariantAction testVariantAction){
+        VariantAction = testVariantAction;
         text_button.text = testVariantAction.ActionName;
+        Index = testVariantAction.number;
+        text_number.text = Index.ToString();
         if(testVariantAction.number == 0)
             return;
         text_number.text = testVariantAction.number.ToString();
-        Set_Color(GameManager.select_color);        
+        Set_Color(GameManager.instance.select_color);        
+    }
+
+    public void button_click(){
+        Set_Color(GameManager.instance.select_color);
+        GameManager.Index_Selected_Element++;
+        Index = GameManager.Index_Selected_Element;
+        VariantAction.number = Index;
+        text_number.text = Index.ToString();
+        Debug.Log("Choice: " + VariantAction.ActionName + "  Index: " + Index);
     }
 
     private void Set_Color(Color color)=>this.GetComponent<Image>().color = color;
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,15 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public static Color select_color;
-    public static Color main_color;
+    public  Color select_color;
+    public  Color main_color;
     [Header("for Menu")]
     [SerializeField] private GameObject menu;
     [SerializeField] private Button[] buttons_roles;
     [SerializeField] private Button buttons_deoloyment;
     [SerializeField] private Button buttons_folding;
+    [SerializeField] private GameObject Panel_Sure_Check;
+    [SerializeField] private GameObject Panel_Check_Result;
 
 
     [Header("for Testing")]
@@ -22,9 +24,17 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Command Action_folding;
     [SerializeField] private GameObject Button_Prefab;
     [SerializeField] private Transform Content;
+    [SerializeField] private Button M1_button;
+    [SerializeField] private Button M2_button;
+    [SerializeField] private Button Outside_button;
+    [SerializeField] private Options M1;
+    [SerializeField] private Options M2;
+    [SerializeField] private Options Outside;
     
     public static GameManager instance;
     private void Awake()=>instance =this;
+
+    public static int Index_Selected_Element;
 
     private int role;    
     private Option_Test test_rls;
@@ -36,14 +46,23 @@ public class GameManager : MonoBehaviour
 
     public void Start(){
         test_rls = Option_Test.none;
+        menu.SetActive(true);
+        Panel_Check_Result.SetActive(false);
+        Panel_Sure_Check.SetActive(false);
+        actions_select.SetActive(false);
     }  
+
+    public void Sure_To_Menu()=>Panel_Sure_Check.SetActive(true);
+    public void Cansel_Menu()=>Panel_Sure_Check.SetActive(false);
 
     public void Back_To_Menu(){
         menu.SetActive(true);
+        Panel_Sure_Check.SetActive(false);
         actions_select.SetActive(false);
-        role = 0;
+        actions_select.SetActive(false);
+        role = 0;        
         test_rls = Option_Test.none;
-        Reset_Color_Button();
+        Reset_Color_Button_Menu();
         Debug.Log("END TEST");
     }
 
@@ -52,12 +71,20 @@ public class GameManager : MonoBehaviour
         if(test_rls == Option_Test.none) return;
         Debug.Log("START TEST");
         Debug.Log("role: "+ role.ToString()+ " test: " + test_rls);
-        Back_To_Menu();
-
         menu.SetActive(false);
         actions_select.SetActive(true);
+        Reset_ALL_Elements();
+        Placement_Outside();
+    }
+
+    private void Pass_Test(){
+
     }
     
+    private void Fall_Test(){
+
+    }
+
     public void Set_Role(int r){        
         role =r;
         foreach (var b in buttons_roles)
@@ -80,36 +107,72 @@ public class GameManager : MonoBehaviour
         Start_Test();
     }
     
-    public void Placement_Button(Options option_list){
-        // while (Content.childCount > 0)
-        //     Destroy(Content.GetChild(0).gameObject);
-        
-        foreach (var opt in option_list.variants_deoloyment)
-        {
-           GameObject buttonInstance = Instantiate(Button_Prefab,Content);
-           Text text_button = buttonInstance.GetComponentInChildren<Text>();
-           text_button.text = opt.ActionName;
-                      
-        }
+    public void Placement_M1()=>Placement_Text_List(M1, M1_button);
+    public void Placement_M2()=>Placement_Text_List(M2,M2_button);
+    public void Placement_Outside()=> Placement_Text_List(Outside, Outside_button);
 
+    public void Placement_Text_List(Options option, Button button_click){ 
+
+        Reset_Color_Button_Navigation();
+        Switch_color(button_click, select_color);
+
+        if(test_rls == Option_Test.is_deoloyment)
+            Initialization_Button(option.variants_deoloyment);
+        else if(test_rls == Option_Test.is_folding){
+            if(option.use_list_deoloyment== true)
+                Initialization_Button(option.variants_deoloyment);
+            else
+                Initialization_Button(option.variants_folding);            
+        }else
+            Debug.Log("ERROR!!!");        
+        Debug.Log("Button is placement, for: " + test_rls);
     }
 
-    private void Reset_Color_Button(){
+    private void Initialization_Button(List<TestVariantAction> variantActions){
+        foreach (Transform child in Content)
+            Destroy(child.gameObject);  
+        foreach (var opt in variantActions)
+        {
+           GameObject buttonInstance = Instantiate(Button_Prefab,Content);
+           buttonInstance.GetComponent<Button_Body>().Get_Variant(opt);                     
+        }     
+    }
+
+    private void Reset_ALL_Elements(){
+        foreach (TestVariantAction v in M1.variants_deoloyment)
+            v.number = 0;
+        foreach(TestVariantAction v in M1.variants_folding)
+            v.number =0;
+        foreach(TestVariantAction v in M2.variants_deoloyment)
+            v.number =0;
+        foreach(TestVariantAction v in M2.variants_folding)
+            v.number = 0;
+        foreach(TestVariantAction v in Outside.variants_deoloyment)
+            v.number = 0;
+        foreach(TestVariantAction v in Outside.variants_folding)
+            v.number =0;
+        Index_Selected_Element = 0;        
+    }
+    
+    private void Reset_Color_Button_Navigation(){
+        Switch_color(M1_button,main_color);
+        Switch_color(M2_button,main_color);
+        Switch_color(Outside_button,main_color);
+    }
+
+    private void Reset_Color_Button_Menu(){
         foreach (var b in buttons_roles)
             Switch_color(b, main_color);        
         Switch_color(buttons_deoloyment, main_color);
         Switch_color(buttons_folding, main_color);     
     }
 
-    private void Switch_color (Button b, Color c)
-    {
-        b.gameObject.GetComponent<Image>().color = c;
+    private void Switch_color (Button b, Color c)=> b.gameObject.GetComponent<Image>().color = c;
         // ColorBlock b_color = b.colors;
         // b_color.normalColor = c;
         // b_color.highlightedColor = c;
         // b_color.pressedColor = c;
         // b.colors = b_color;
-    }
-
+    
     public void Exit()=>Application.Quit();
 }
